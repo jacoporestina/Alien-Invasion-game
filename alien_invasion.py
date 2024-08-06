f37d77aa713353c87a8ac7d7e4cc269d4303beac
JacopoRestina <jacopo.restina@wur.nl>
2024-08-06 19:32:21 +0200

    Alien invasion game almost completed, only missing the last exercises and the game can be launched.

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from ship import Ship
 from bullet import Bullet
 from alien import Alien
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 
 class AlienInvasion:
@@ -21,8 +22,10 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
-        # Create an instance to store game statistics.
+        # Create an instance to store game statistics, 
+        # and creat a scoreboard.
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -71,6 +74,9 @@ class AlienInvasion:
         # Reset game statistics
         self.stats.reset_stats()
         self.stats.game_active = True
+        self.sb.prep_score()
+        self.sb.prep_level()
+        self.sb.prep_ships()
 
         # Get rid of any remaining aliens and bullets
         self.aliens.empty()
@@ -126,6 +132,12 @@ class AlienInvasion:
         """Respond to bullet-alien collision."""
         # Remove bullet and alien that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
         
         # Repopulate the fleet of aliens.
         if not self.aliens:
@@ -133,10 +145,13 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
     
     def _update_aliens(self):
-        """
-        Check if the fleet is at the edge, and
+        """Check if the fleet is at the edge, and
         update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
@@ -200,9 +215,10 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
-        if self.stats.ship_left > 0:
-            # Decrement ships_left.
+        if self.stats.ship_left > 1:
+            # Decrement ships_left, and update scoreboard.
             self.stats.ship_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remainig aliens and bullets.
             self.aliens.empty()
@@ -225,6 +241,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+
+        # Draw the score information.
+        self.sb.show_score()
         
         # Draw the play button if the game is inactive
         if not self.stats.game_active:
--- settings.py
+++ settings.py
@@ -23,20 +23,28 @@ class Settings:
         # How quickly the game speeds up
         self.speedup_scale = 1.1
 
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+
         self.initialize_dinamic_settings()
 
     def initialize_dinamic_settings(self):
         """Initialize settings that can change during the game"""
         self.ship_speed = 1.5
         self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        self.alien_speed = 0.2
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
+        # Scoring
+        self.alien_points = 50
+
     def increase_speed(self):
-        """Increase speed settings"""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
 
+        self.alien_points = int(self.alien_points * self.score_scale)
+        #print(self.alien_points)
